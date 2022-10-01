4698db0810a394b72bc2d2cb0fdbb5e5e2ef8ad8
unknown <perecpon@yandex.ru>
2022-10-01 14:36:42 +0300

    LenghtArray

---
+++ example29/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, в которой пользователь задает длину массива,
+элементы которого задаются в диапазоне [1,99] и выводит на экран .
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]*/
+
+int ReadInt(string number)
+{
+    Console.Write(number);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int LengthArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[LengthArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1, 99);
+    //Console.Write(randomArray[i] + " ");
+}
+printArray(randomArray);
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i < array.Length -1)
+        {
+        Console.Write(",");  
+        }   
+    }
+    Console.WriteLine("]");
+}
